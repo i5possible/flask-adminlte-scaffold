6c624632b45732a6748fe94be003fd89833ff807
Liang Hong <lianghongbuaa@gmail.com>
2018-06-06 17:44:11 +0800

    add write api

--- app/main/edit_config.py
+++ app/main/edit_config.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request, jsonify, flash
 
 from app import get_logger, get_config
 from . import main
@@ -8,9 +8,15 @@ logger = get_logger(__name__)
 cfg = get_config()
 
 
-@main.route('/api/project/config', methods=['GET', 'POST'])
-def p_config():
-    file = request.args.get("path", "resources/file1.txt")
+@main.route('/api/project/config', methods=['GET'])
+def read_config():
+    """
+    :path_param path: the path of config file to read
+    :return: {
+        "config_content": "String with '\n'"
+    }
+    """
+    file = request.args.get("path")
     print(file)
     abspath = path.abspath(file)
     print(abspath)
@@ -21,3 +27,25 @@ def p_config():
             lines.append(line)
         print(lines)
     return jsonify({"config_content": "".join(lines)})
+
+
+@main.route('/api/project/config', methods=['POST'])
+def write_config():
+    """
+    :path_param path: the path of config file to read
+    :json_content {
+        "config_content": "String with '\n'"
+    }
+    :return:
+    """
+    file = request.args.get("path", "resources/file3.txt")
+    content = request.json["config_content"]
+    print(content)
+
+    abspath = path.abspath(file)
+    print(abspath)
+
+    with open(abspath, 'w') as config_file:
+        config_file.write(content)
+    flash("Config is Saved!")
+    return "Config is Saved!", 201
